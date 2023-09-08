eb3bfde52d74954e33d8cda28991a44d55a4fdda
bhavyb <144156101+bhavyb@users.noreply.github.com>
2023-09-08 13:06:18 +0530

    Add files via upload

---
+++ p2.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+class temp
+{
+    public:
+        static int n;
+};
+int temp::n=15;
+int main()
+{
+    int n=22;
+    cout<<"Use of scope resolution operator with class "<<endl;
+    cout<<"value stored in a using class is :: "<<temp::n<<endl;
+    cout<<"value stored in a using class is :: "<<n<<endl;
+    return 0;
+}
